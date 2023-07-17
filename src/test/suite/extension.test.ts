6f444260815c8fe499e02a8bc8a6c096c78fbb86
Wilhelm Klopp <wil.klopp@gmail.com>
2023-07-17 11:37:58 +0100

    init

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,25 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../../extension';
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Sample test', async () => {
+		const ext = vscode.extensions.getExtension("test.encryption-not-available-demo");
+		if (!ext) {
+			throw new Error("Extension not found");
+		}
+
+		const context: vscode.ExtensionContext = await ext.activate();
+
+		assert.ok(ext.isActive);
+
+		context.secrets.store("test", "test");
+
+		assert.strictEqual(1+1,2);
+	});
+});

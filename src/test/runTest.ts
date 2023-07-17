760eb41983c65d6aa6bd3b9c3a8a170a6c9ddb9d
Wilhelm Klopp <wil.klopp@gmail.com>
2023-07-17 12:08:59 +0100

    Disable gpu

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,7 +13,7 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ version: "1.79.2", extensionDevelopmentPath, extensionTestsPath });
+		await runTests({ version: "1.79.2", extensionDevelopmentPath, extensionTestsPath, launchArgs: ["--disable-gpu"] });
 	} catch (err) {
 		console.error('Failed to run tests', err);
 		process.exit(1);
